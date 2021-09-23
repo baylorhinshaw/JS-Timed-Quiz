388f25cc1e0906c484cdc058fcef777010fc5f28
Baylor <baylorhinshaw21@gmail.com>
2021-09-23 00:06:55 -0400

    added local storage and got next ans options to show and also if the users answer is incorrect or correct

--- assets/script.js
+++ assets/script.js
@@ -1,113 +1,179 @@
-const timeEl = document.querySelector('.time');
+let timeEl = document.querySelector('.time');
 let score = 0;
 let currentQuestion = 0;
-const beginQuizBtn = document.querySelector('#begin-quiz');
-const questionsContainerEl = document.querySelector('#questions-box');
-let answer1 = document.getElementById("answer1")
-let answer2 = document.getElementById("answer2")
-let answer3 = document.getElementById("answer3")
-let answer4 = document.getElementById("answer4")
+let secondsLeft = 60;
+let beginQuizBtn = document.querySelector('#begin-quiz');
+let questionsContainerEl = document.querySelector('#questions-box');
+let questionContainerEl = document.getElementById('ans-btns')
+let questionTitleEl = document.getElementById('question-display');
 let done = document.querySelector('.done');
 let goBack = document.querySelector('#end-btn');
-let resultCont = document.getElementById('result')
+let resultCont = document.getElementById('result');
+let newEl;
 
-const qna = [{ 
+const qna = [{
     question: 'What year was JavaScript created?',
-    ans1: '1995',
-    ans2: '1969',
-    ans3: '1977',
-    ans4: '2012',
-    correctAns: '1995'
+    options: [
+        '1995',
+        '1969',
+        '1977',
+        '2012'
+    ],
+    answer: 0,
 },
 {
     question: 'Which is not a JS variable?',
-    ans1: 'let',
-    ans2: 'const',
-    ans3: 'null',
-    ans4: 'var',
-    correctAns: 'null'
+    options: [
+        'let',
+        'const',
+        'null',
+        'var'
+    ],
+    answer: 2,
 },
 {
     question: 'What describes JS the best?',
-    ans1: 'programs the behavior of web pages',
-    ans2: 'defines the content of web pages',
-    ans3: 'specifies the layout of webpages',
-    ans4: 'none of the above',
-    correctAns: 'programs the behavior of web pages'
+    options: [
+        'programs the behavior of web pages',
+        'defines the content of web pages',
+        'specifies the layout of webpages',
+        'none of the above'
+    ],
+    answer: 0,
 },
 {
     question: 'Which character is used to make an array in JS?',
-    ans1: '()',
-    ans2: '{}',
-    ans3: '[]',
-    ans4: '<>',
-    correctAns: '[]'
+    options: [
+        '()',
+        '{}',
+        '[]',
+        '<>'
+    ],
+    answer: 2,
 },
 {
     question: 'What would you use to generate a random number?',
-    ans1: 'Math.random',
-    ans2: 'Math.floor',
-    ans3: 'Math.wild',
-    ans4: 'none of the above',
-    correctAns: 'Math.random'
-},    
+    options: [
+        'Math.random',
+        'Math.floor',
+        'Math.wild',
+        'none of the above'
+    ],
+    answer: 0,
+},
 {
     question: 'How do you comment in JS?',
-    ans1: '!!',
-    ans2: '==',
-    ans3: '//',
-    ans4: '$$',
-    correctAns: '//'
+    options: [
+        '!!',
+        '==',
+        '//',
+        '$$'
+    ],
+    answer: 2,
 }
 ];
-    
-    function beginQuiz () { 
-        quizTime();
-        beginQuizBtn.classList.add('hide');
-        questionsContainerEl.classList.remove('hide');
-        const questionEl = document.getElementById('question-display');
-        questionEl.textContent = qna[0].question;
-        answer1.textContent = qna[0].ans1;
-        answer2.textContent = qna[0].ans2;
-        answer3.textContent = qna[0].ans3;
-        answer4.textContent = qna[0].ans4;
+
+function beginQuiz() {
+    quizTime();
+    beginQuizBtn.classList.add('hide');
+    questionsContainerEl.classList.remove('hide');
+
+
+    let firstQuestion = qna[0];
+    let options = firstQuestion.options;
+    let title = firstQuestion.question;
+
+    questionTitleEl.textContent = title;
+
+    for (let i = 0; i < options.length; i++) {
+        let answer = options[i];
+        let newEl = document.createElement('button');
+        newEl.className = 'ans-btn';
+        newEl.textContent = answer;
+        questionsContainerEl.append(newEl);
+
+        if (newEl) {
+            let nextQuestion = 1;
+            newEl.addEventListener('click', function (e) {
+                correctOrIncorrectAnswerSelected(answer, options[firstQuestion.answer], nextQuestion)
+            })
+        }
+    }
 }
+
+
 
 beginQuizBtn.addEventListener('click', beginQuiz);
 
-    function correctAnswerSelected () {
-        let answerButtonElements = document.getElementsByClassName('ans-btn'); 
-        for (let i = 0; i < answerButtonElements.length; i++) {
 
-        }
+function correctOrIncorrectAnswerSelected(selectedAnswer, correctAnswer, nextQuestion) {
+    let answerButtonElements = document.getElementsByClassName('ans-btn');
+    if (selectedAnswer === correctAnswer) {
+        document.getElementById('result').innerHTML = 'You answered correctly!'
+        score += 1;
+        nextQuestion = nextQuestion++;
+        newQuestionDisplayed(nextQuestion);
+    } else {
+        document.getElementById('result').innerHTML = 'You answered incorrectly!'
+        secondsLeft -= 5;
+        timeEl.textContent = secondsLeft + 's to finsh!';
+        nextQuestion = nextQuestion++;
+        newQuestionDisplayed(nextQuestion);
     }
-
-    function incorrectAnswerSelected () {
-        
-    }
-
-    function newQuestionDisplayed () {
-        let displayNewQuestion = document.getElementsByClassName('next-question');
-        for (let i = 0; i < displayNewQuestion.length; i++) {
-            
-        }
-    }
-
-
-
-function finisedQuiz () {
-    done.classList.remove('hide');
-    goBack.classList.remove('hide');
 }
 
-function quizTime () {
-    let secondsLeft = 60;
-    let timerInterval = setInterval(function() {
+
+function newQuestionDisplayed(nextQuestion) {
+    document.querySelector('#questions-box').innerHTML = '';
+    questionTitleEl.innerHTML = '';
+    let displayQuestion = qna[nextQuestion];
+    let options = displayQuestion.options;
+    questionTitleEl.textContent = displayQuestion.question;
+    for (let i = 0; i < options.length; i++) {
+        let answer = options[i];
+        let newEl = document.createElement('button');
+        newEl.className = 'ans-btn';
+        newEl.textContent = answer;
+        questionsContainerEl.append(newEl);
+        if (newEl) {
+            console.log(nextQuestion);
+            newEl.addEventListener('click', function (e) {
+                correctOrIncorrectAnswerSelected(answer, options[displayQuestion.answer], nextQuestion)
+    
+            })
+        }
+    }
+    
+}
+
+
+function quizTime() {
+    let timerInterval = setInterval(function () {
         secondsLeft--;
         timeEl.textContent = secondsLeft + 's to finish!';
-        
-        if(secondsLeft === 0) {
+
+        if (secondsLeft === 0) {
             clearInterval(timerInterval);
+            alert('Quiz over');
         }
-    }, 1000);    
+    }, 1000);
 }
+
+let initialsInput = document.querySelector("#initials");
+let submitButton = document.querySelector("submit")
+
+function renderLastInitial () {
+    var initials = localStorage.getItem("initials")
+}
+
+submitButton.addEventListener("click", function(event) {
+    event.preventDefault();
+})
+
+localStorage.setItem("initials", initialsInput);
+renderLastInitial;
+
+
+// function finisedQuiz () {
+//     done.classList.remove('hide');
+//     goBack.classList.remove('hide');
